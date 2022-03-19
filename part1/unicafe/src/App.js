335a250ed848b1d208ae223605d1af0e7d67303d
congbk92 <thanhcong.bk10@gmail.com>
2022-03-19 19:39:30 +0700

    1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,27 +6,33 @@ const Button = ({onClick, text}) => {
   )
 }
 
+const StatisticLine = ({text, value}) => {
+  return (
+    <div>
+      <p>{text} {value}</p>
+    </div>
+  )
+}
+
 const Statistics  = ({good, neutral, bad}) => {
   const all = good + neutral + bad
   const average = (good-bad)/all
-  const positive = (good*100)/all
+  const positive = (good*100)/all + " %"
   if (all === 0){
     return (
       <div>
-        <h1>statistics</h1>
         <p>No feedback given</p>
       </div>
     )
   } else {
     return (
       <div>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>avarage {average}</p>
-        <p>positive {positive} %</p>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={all}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={positive}/>
       </div>
     )
   }
@@ -48,6 +54,7 @@ const App = () => {
       <Button onClick={onClickGood} text="good"/>
       <Button onClick={onClickNeutral} text="neutral"/>
       <Button onClick={onClickBad} text="bad"/>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
