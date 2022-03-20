22d56e41b25a6c81417688119711a9bdb7e437e7
congbk92 <thanhcong.bk10@gmail.com>
2022-03-20 22:25:18 +0700

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,12 @@ const App = () => {
   const [persons, setPersons] = useState([
     { 
       name: 'Arto Hellas',
+      number: '000-000-111',
       id: 0
     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPeople = (event) => {
     event.preventDefault()
@@ -17,15 +19,21 @@ const App = () => {
     } else {
       const people = {
         name: newName,
+        number: newNumber,
         id: persons.length
       }
       setPersons(persons.concat(people))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,11 +44,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(people => <p key={people.id}>{people.name}</p>)}
+      {persons.map(people => <p key={people.id}>{people.name} {people.number}</p>)}
       <h1>debug: {newName}</h1>
     </div>
   )
