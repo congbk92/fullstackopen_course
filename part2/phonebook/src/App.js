7a8fdd2c20b2375dbc6b7255b50b2612c6f7e3e0
congbk92 <thanhcong.bk10@gmail.com>
2022-03-20 23:00:57 +0700

    2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,44 @@
 import { useState } from 'react'
 
+
+const Input = ({text, value, handleOnChange}) => {
+  return (
+    <div>
+      {text} <input value={value} onChange={handleOnChange}/>
+    </div>
+  )
+}
+
+const AddForm = ({onSubmit, newName, handleNameChange, newNumber, handleNumberChange}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <h2>Add a new</h2>
+      <Input text="name:" value={newName} handleOnChange={handleNameChange}/>
+      <Input text="number:" value={newNumber} handleOnChange={handleNumberChange}/>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const ShowPeople = ({people}) => {
+  return (
+    <div>
+      <p>{people.name} {people.number}</p>
+    </div>
+  )
+}
+
+const ShowPhoneBook = ({personsToShow}) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {personsToShow.map(people => <ShowPeople key={people.id} people={people}/>)}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -45,23 +84,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPeople}>
-        <div>
-          filter shown with: <input value={showWith} onChange={handleShowWithChange}/>
-        </div>
-        <h2>Add a new</h2>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personsToShow.map(people => <p key={people.id}>{people.name} {people.number}</p>)}
+      <Input text="filter shown with:" value={showWith} handleOnChange={handleShowWithChange}/>
+      <AddForm onSubmit={addPeople} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+      <ShowPhoneBook personsToShow={personsToShow}/>
     </div>
   )
 }
