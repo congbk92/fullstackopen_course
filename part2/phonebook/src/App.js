6e46789817d33b7c772cb32d29fed35fd2c0a286
congbk92 <thanhcong.bk10@gmail.com>
2022-03-21 21:49:24 +0700

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Input = ({text, value, handleOnChange}) => {
   return (
@@ -40,15 +40,20 @@ const ShowPhoneBook = ({personsToShow}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showWith, setNewShowWith] = useState('')
+
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(respone => {
+        console.log('promise fulfilled')
+        setPersons(respone.data)
+      })
+  }, [])
 
   const addPeople = (event) => {
     event.preventDefault()
