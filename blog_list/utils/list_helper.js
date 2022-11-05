d25cffb0d28c4278bf74506357cc6d0892435ad3
congbk92 <thanhcong.bk10@gmail.com>
2022-11-05 14:40:30 +0700

    4.4: helper functions and unit tests, step2

--- blog_list/utils/list_helper.js
+++ blog_list/utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.map(blog => blog.likes)
+              .reduce((acc, val) => {return acc + val}, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
